1b5c9ea842ffe12681db0c41c39da82044e54adf
darkwoolf <woolftreuser@gmail.com>
2019-08-07 15:07:06 +0300

    added global gotham font

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkwoolf: {
+        area: 'frontend',
+        name: 'Darkwoolf/Dark',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    }
+};
