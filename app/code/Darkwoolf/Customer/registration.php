bc7d5138642c53b1d8b4aec2030e28874c022993
darkwoolf <woolftreuser@gmail.com>
2019-08-09 12:06:43 +0300

    added module customer and Registration for dealer block

---
+++ app/code/Darkwoolf/Customer/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Darkwoolf_Customer',
+    __DIR__
+);
---
+++ app/code/Darkwoolf/Customer/view/frontend/requirejs-config.js
@@ -0,0 +1,5 @@
+var config = {
+    paths: {
+        dealerModal: 'Darkwoolf_Customer/js/dealer-modal',
+    }
+};
