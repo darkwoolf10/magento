8254367721f3c715033ff4e4f573a9d05d014dab
darkwoolf <woolftreuser@gmail.com>
2019-08-22 15:52:02 +0300

    fix problems

--- app/code/Darkwoolf/CustomerRegisterDealer/view/frontend/requirejs-config.js
+++ app/code/Darkwoolf/CustomerRegisterDealer/view/frontend/requirejs-config.js
@@ -1,8 +1,5 @@
 var config = {
     paths: {
         dealerModal: 'Darkwoolf_CustomerRegisterDealer/js/dealer-modal',
-    },
-    deps: [
-        'Darkwoolf_CustomerRegisterDealer/js/dealer-modal'
-    ]
+    }
 };
