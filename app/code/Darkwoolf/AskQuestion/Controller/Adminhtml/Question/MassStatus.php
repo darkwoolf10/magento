6c3e691f2d055d59a75dd50b930ff10c13a5a75d
darkwoolf <woolftreuser@gmail.com>
2019-08-23 13:52:43 +0300

    fix admin grid()

--- app/code/Darkwoolf/AskQuestion/Controller/Adminhtml/Question/MassStatus.php
+++ app/code/Darkwoolf/AskQuestion/Controller/Adminhtml/Question/MassStatus.php
@@ -4,7 +4,8 @@ namespace Darkwoolf\AskQuestion\Controller\Adminhtml\Question;
 
 use \Magento\Backend\Block\Template\Context;
 use Darkwoolf\AskQuestion\Model\ResourceModel\Question\CollectionFactory;
-use Magento\Eav\Model\Entity\Collection\AbstractCollection;
+use \Magento\Backend\App\Action;
+use Magento\Framework\App\ResponseInterface;
 use Magento\Ui\Component\MassAction\Filter;
 use Magento\Framework\Controller\ResultFactory;
 use Darkwoolf\AskQuestion\Model\ResourceModel\Question\Collection;
@@ -13,38 +14,60 @@ use Darkwoolf\AskQuestion\Model\ResourceModel\Question\Collection;
  * Class MassStatus
  * @package Darkwoolf\AskQuestion\Controller\Adminhtml\Question
  */
-class MassStatus extends AbstractMassAction
+class MassStatus extends Action
 {
     /**
      * MassStatus constructor.
      * @param Context $context
      * @param Filter $filter
-     * @param \Darkwoolf\AskQuestion\Model\ResourceModel\Question\CollectionFactory $collectionFactory
+     * @param CollectionFactory $collectionFactory
      */
-    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory) {
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        Filter $filter,
+        CollectionFactory $collectionFactory
+    ) {
         parent::__construct($context);
     }
 
     /**
-     * @param AbstractCollection $collection
-     * @return \Magento\Backend\Model\View\Result\Redirect
+     * @param Collection $collection
+     * @return \Magento\Framework\Controller\ResultInterface
+     * @throws \Exception
      */
     protected function massAction(Collection $collection)
     {
-        $questionChangeStatus = 0;
-
+        /** @var \Darkwoolf\AskQuestion\Model\Question $rate */
         foreach ($collection as $rate) {
-            $rate->setStatus('Answered')->save();
-            $questionChangeStatus++;
+            $rate->setStatus('Answered');
         }
 
-        if ($questionChangeStatus) {
-            $this->messageManager->addSuccess(__('A total of %1 record(s) were updated.', $questionChangeStatus));
+        $collection->save();
+
+        if (count($collection)) {
+            $this->messageManager->addSuccess(__('A total of %1 record(s) were updated.', count($collection)));
         }
 
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
         $resultRedirect->setPath($this->getComponentRefererUrl());
 
         return $resultRedirect;
+    }
+
+    /**
+     * Execute action based on request and return result
+     *
+     * Note: Request will be added as operation argument in future
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->getConfig()->getTitle()->prepend(__('Customers questions'));
+
+        return $resultPage;
     }
 }
